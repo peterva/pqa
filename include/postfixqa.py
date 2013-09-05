8655a65c2bc6f406ca60388d98741fb855a65a53
Peter van Arkel <p.vanarkel@gmail.com>
2013-09-05 10:32:26 +0200

    Temp upload to github

---
+++ include/postfixqa.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+# Author    : Peter van Arkel <p.vanarkel@gmail.com>
+# License   : GPLv3
+# Version   : 0.1
+# Codebase  : http://repo.vasercon.nl:8080/gitlab/pqa/tree/master
+
+"""Postfix Queue Administration Tool"""
+
